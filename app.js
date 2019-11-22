d0abc0a7aab729b31ddd07d413213d4c5e059d6f
Dipesh Lamichhane <ft9dipesh@gmail.com>
2019-11-22 15:41:32 +0545

    Set Up environment variables

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: 'dipesh-session',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
